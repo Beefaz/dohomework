37bff91e170782a3cc1640557da7033f8e9a1d49
Beefaz <justinas.geryba@gmail.com>
2021-02-03 03:37:55 +0200

    created working table with fetched data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <DataTable/>
+        <table v-if="list!==undefined">
+            <tr>
+                <th v-for="header in Object.getOwnPropertyNames(list[0])" v-bind:key="header.index">
+                    {{header}}
+                </th>
+            </tr>
+            <tr v-for="row in list" v-bind:key="row.index">
+                <td v-for="column in row" v-bind:key="column.index">
+                    {{column}}
+                </td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import DataTable from './components/DataTable.vue'
+    import Vue from 'vue';
+    import axios from 'axios';
+    import VueAxios from 'vue-axios';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    Vue.use(VueAxios, axios);
+
+    export default {
+        name: 'App',
+        components: {
+            DataTable
+        },
+        data() {
+            return {list: undefined}
+        },
+
+        async mounted() {
+            Vue.axios.get('https://disease.sh/v3/covid-19/countries')
+                .then((response) => {
+                    this.list = response.data;
+                })
+                .catch((error) => {
+                    console.warn(error);
+                })
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 </style>
+
---
+++ src/components/DataTable.vue
@@ -0,0 +1,22 @@
+<script>
+    export default {
+        name: 'DataTable',
+        components:'',
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .dataTable {
+        display:flex;
+        width: 100%;
+        text-align: center;
+    }
+</style>
+
+
+<template>
+    <div class="dataTable">
+        <button>GetData</button>
+    </div>
+</template>
